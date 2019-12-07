7e2a1c40d3d7c13637881c62decb8a9760bbd751
vacit <wajdabdulrazak@gmail.com>
2019-12-07 16:42:45 +0300

    Updates

---
+++ precache-manifest.7b1a6c6256a8cb852d56a88cd7cb06fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a81fa8cdd2d05aa2838a85308a65a6b",
+    "url": "/React-Redux-Todo-App/index.html"
+  },
+  {
+    "revision": "1c5a4963251b194286e1",
+    "url": "/React-Redux-Todo-App/static/css/main.58399f5c.chunk.css"
+  },
+  {
+    "revision": "047f8ba48e24cbd49fdb",
+    "url": "/React-Redux-Todo-App/static/js/2.3a1b8217.chunk.js"
+  },
+  {
+    "revision": "1c5a4963251b194286e1",
+    "url": "/React-Redux-Todo-App/static/js/main.9b49c48e.chunk.js"
+  },
+  {
+    "revision": "3235d89178c7c6533435",
+    "url": "/React-Redux-Todo-App/static/js/runtime-main.8f91c902.js"
+  }
+]);
